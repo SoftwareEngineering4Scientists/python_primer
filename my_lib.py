d135b22734ed9d39e5636aa1cfa290b9043ca33b
Jacob Stanley <jtstan@gmail.com>
2021-08-16 13:34:22 -0600

    Create my_lib.py

---
+++ my_lib.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+  assert RuntimeError("This module is intended only for import.")
+
+def check_equal(a, b, precision = 0.001):
+    is_equal = abs(a - b) < precision
+    return is_equal
