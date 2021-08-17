fdbbb85e2f222e24323bdea522cd2a54fab60904
Jacob Stanley <jtstan@gmail.com>
2021-08-16 20:23:22 -0600

    Update numpy_import.py

--- numpy_import.py
+++ numpy_import.py
@@ -2,4 +2,4 @@
 import numpy as np
 
 rand_integers = np.random.randint(0, 10, size=20)
-print("Here are 20 random integers between 0 and 10:", rand_integers)
+print("Twenty random ints from 0 and 10:", rand_integers)
