963da68c69224175e5ddb31b871059a22d8097a4
Jacob Stanley <jtstan@gmail.com>
2021-08-16 20:06:01 -0600

    Update numpy_import.py

--- numpy_import.py
+++ numpy_import.py
@@ -1,4 +1,5 @@
-Import numpy as np
+# import function can also use 'as' to provide an abbreviation for the package
+import numpy as np
 
 rand_integers = np.random.randint(0, 10, size=20)
 print("Here are 20 random integers between 0 and 10:", rand_integers)
